b7b3337802b335eb34d0f18898d194ef5a06905e
Souzen Khan <souzenk@uci.edu>
2025-07-02 11:21:40 -0700

    Search Bar with functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import { useState } from "react";
+import { use, useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import SearchBar from "./components/SearchBar";
 
 // Code for List Group component
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const allItems = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [filteredItems, setFilteredItems] = useState(allItems);
+
+  // Function for checking if the item is in the list from the navbar
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
-  /*
-``// this would go inside the return statement of the App component
-        <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      /> 
 
-  */
+  const handleSearch = (query: string) => {
+    const lower = query.toLowerCase();
+    setFilteredItems(
+      allItems.filter((item) => item.toLowerCase().includes(lower))
+    );
+  };
+
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
       <div>
+        <SearchBar onSearch={handleSearch} />
+      </div>
+
+      <div>
         <ListGroup
-          items={items}
+          items={filteredItems}
           heading="Cities"
           onSelectItem={handleSelectItem}
         />
