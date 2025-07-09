e048525f12081f29f2319d3c9fae248c098ce0c7
kyuterose <arwa.arsalan@gmail.com>
2025-07-09 15:38:05 -0400

    define models for the db schema

--- backend/meetings/admin.py
+++ backend/meetings/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Meeting
+from .models import Meeting, TimeOption, AvailabilityResponse, AvailabilityEntry
 
 admin.site.register(Meeting)
+admin.site.register(TimeOption)
+admin.site.register(AvailabilityResponse)
+admin.site.register(AvailabilityEntry)
 
