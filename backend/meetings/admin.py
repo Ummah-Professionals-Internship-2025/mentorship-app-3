d0fd318b2044c7a896118bf917dd229dff6b349e
Ameerah <ameerahgadatia17@gmail.com>
2025-07-10 12:22:16 -0400

    added search and list filters and string methods

--- backend/meetings/admin.py
+++ backend/meetings/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Meeting, TimeOption, AvailabilityResponse, AvailabilityEntry
 
-admin.site.register(Meeting)
-admin.site.register(TimeOption)
-admin.site.register(AvailabilityResponse)
-admin.site.register(AvailabilityEntry)
+@admin.register(Meeting)
+class MeetingAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'created_at']
 
+@admin.register(TimeOption)
+class TimeOptionAdmin(admin.ModelAdmin):
+    list_display = ['meeting', 'start_time', 'end_time']
+
+@admin.register(AvailabilityResponse)
+class AvailabilityResponseAdmin(admin.ModelAdmin):
+    search_fields = ['participant_name']
+    list_display = ['participant_name', 'meeting', 'created_at']
+
+@admin.register(AvailabilityEntry)
+class AvailabilityEntryAdmin(admin.ModelAdmin):
+    list_display = ['availability_response', 'time_option']
