4b0eb4d6e683441163da095ad7d2ee2b7bff0b94
kyuterose <arwa.arsalan@gmail.com>
2025-07-17 12:25:39 -0400

    adding meeting form

---
+++ backend/meetings/serializers.py
@@ -0,0 +1,9 @@
+from .models import Meeting
+from rest_framework import serializers
+
+class MeetingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meeting
+        fields = ['id', 'name', 'description', 'created_at']
+        
+
